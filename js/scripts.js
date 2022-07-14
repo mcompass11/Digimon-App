4cac80a98008bcdfb83d002a37753f6d47238519
Monkendrick <52892395+mcompass11@users.noreply.github.com>
2022-07-13 20:15:30 -0400

    added loop for digimon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
-alert('Hello Kendrick');
+let digimonList = [
+  {
+    name: 'Agumon',
+    img: 'http://digimon.shadowsmith.com/img/agumon.jpg',
+    level: 'Rookie'
+  },
+  {
+    name: 'Kuwagamon',
+    img: 'https://digimon.shadowsmith.com/img/kuwagamon.jpg',
+    level: 'Champion'
+  },
+  {
+    name: 'Andromon',
+    img: 'https://digimon.shadowsmith.com/img/andromon.jpg',
+    level: 'Ultimate'
+  }
+];
 
-let favoriteFood = 'Wings';
 
-document.write(favoriteFood);
+  for(let i=0; i< digimonList.length; i++) {
+    if(digimonList[i].level === 'Ultimate') {
+      document.write(' Whoa, ' + digimonList[i].name + ' is strong!')
+    } else {
+      document.write(' ' + digimonList[i].name)
+    }
+  }
